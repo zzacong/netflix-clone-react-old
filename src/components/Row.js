08c2555ff93d85faa747b46ef052be15fba27e48
zzacong <ongzz100@gmail.com>
2020-10-22 15:12:28 +1100

    youtube trailer and firebase init

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react'
+import YouTube from 'react-youtube'
+import movieTrailer from 'movie-trailer'
 
 import axios from '../axios'
 import './css/Row.css'
 
 export default function Row({ title, url, isLargeRow }) {
   const [movies, setMovies] = useState([])
+  const [trailerUrl, setTrailerUrl] = useState('')
   const imageBaseUrl = 'https://image.tmdb.org/t/p/original'
 
   useEffect(() => {
@@ -18,6 +21,31 @@ export default function Row({ title, url, isLargeRow }) {
 
   // console.table(movies)
 
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      autoplay: 1,
+    },
+  }
+
+  const handleClick = async movie => {
+    if (trailerUrl) {
+      setTrailerUrl('')
+    } else {
+      try {
+        const url = await movieTrailer(
+          movie?.original_name || movie?.title || movie?.name || ''
+        )
+        console.log('url: ', url)
+        const urlParams = new URLSearchParams(new URL(url).search)
+        setTrailerUrl(urlParams.get('v'))
+      } catch (error) {
+        console.log('movie trailer error:', error)
+      }
+    }
+  }
+
   return (
     <div className="row">
       <h2>{title}</h2>
@@ -26,6 +54,7 @@ export default function Row({ title, url, isLargeRow }) {
           return (
             <img
               className="row_poster"
+              onClick={() => handleClick(movie)}
               key={movie.id}
               src={`${imageBaseUrl}${
                 isLargeRow ? movie.poster_path : movie.backdrop_path
@@ -35,6 +64,7 @@ export default function Row({ title, url, isLargeRow }) {
           )
         })}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   )
 }
