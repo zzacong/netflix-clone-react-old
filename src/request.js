7c50afbfff931b08b1ab403eb4cc3d4a8179ef97
zzacong <ongzz100@gmail.com>
2020-12-08 22:56:45 +1100

    fix top rated

--- src/request.js
+++ src/request.js
@@ -3,7 +3,7 @@ const API_KEY = process.env.REACT_APP_TMDB_API_KEY
 const requests = {
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
   fetchTrending: `trending/all/week?api_key=${API_KEY}`,
-  fetchTopRated: `tv/top_rated?api_key=${API_KEY}`,
+  fetchTopRated: `movie/top_rated?api_key=${API_KEY}`,
   fetchActionTV: `discover/tv?api_key=${API_KEY}&with_genres=10759`,
   fetchMysteryTV: `discover/tv?api_key=${API_KEY}&with_genres=9648`,
   fetchDramaTV: `discover/tv?api_key=${API_KEY}&with_genres=18`,
